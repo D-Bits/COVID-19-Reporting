2d89bb33659c1d4d1883d28dfd93c1dd454ad983
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-03-29 13:48:59 -0700

    Implemented ETL, and updated models.

--- reporting/reports/models.py
+++ reporting/reports/models.py
@@ -1,4 +1,16 @@
 from django.db import models
 
 
-# Create your models here.
+# Model for data from summary API
+class Summary(models.Model):
+
+    Country = models.CharField(max_length=255)
+    Slug = models.CharField(max_length=255)
+    NewConfirmed = models.IntegerField()
+    TotalConfirmed = models.IntegerField()
+    NewDeaths = models.IntegerField()
+    TotalDeaths = models.IntegerField()
+    NewRecovered = models.IntegerField()
+    TotalRecovered = models.IntegerField()
+
+
