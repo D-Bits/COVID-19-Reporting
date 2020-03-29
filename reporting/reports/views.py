1bfd3c379445746cd7a1157bef4c25f243e6881c
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-03-29 14:06:35 -0700

    Added IndexView

--- reporting/reports/views.py
+++ reporting/reports/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Summary
 
-# Create your views here.
+
+# List all data from the Summary model
+class IndexView(ListView):
+
+    model = Summary
+    ordering = ['-Country']
+    template_name = "reports/base.html"
